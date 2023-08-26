2a7bfa6943d8735df4936794d724904d863c631f
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2023-08-26 01:48:57 -0400

    Partially updated authentication and user login

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth"
+
+import { authOptions } from "@/lib/session"
+import nextAuth from "next-auth"
+
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { NavLinks } from '@/constants'
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from "@/lib/session";
 
 
-const Navbar = () => {
-
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className='flexBetween navbar'>
@@ -28,9 +28,19 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='flexCenter gap-4'>
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user?.image
+              && (
+                <Image
+                  src={session.user.image}
+                  width={40}
+                  height={40}
+                  className='rounder-full'
+                  alt={session.user.name}
+                />
+              )
+            }
 
             <Link href="/create-project">
               Share Work
